32da1655be1ebd2388722514ddae641f155f3b18
Beztix <sebastian.luetzow@gmail.com>
2023-02-03 23:38:29 +0100

    add card placement check

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(menuName = "Card")]
 public class Card : ScriptableObject
 {
+    public string cardType;
+    public int numberOfSockets;
+    public string[] attachableCardTypes;
+
+
+    public bool CanAttachCardType(string cardType)
+    {
+        if (attachableCardTypes.Contains(cardType))
+        {
+            return true;
+        }
+        return false;
+    }
 }
--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class CardView : MonoBehaviour
 {
-    
+    private Card _card;
+
+    private int _occupiedSockets = 0;
+
+    public Card GetCard()
+    {
+        return _card;
+    }
+
+    public bool HasOpenSockets()
+    {
+        if (_occupiedSockets < _card.numberOfSockets)
+        {
+            return true;
+        }
+        return false;
+    }
 }
